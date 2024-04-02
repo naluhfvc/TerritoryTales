984af968c251f764aaef8cac1b32e97b909dea7c
Jhonas Gomes <jhowscts@gmail.com>
2024-04-01 21:33:19 -0300

    inicio colocacao personagem

--- src/view/Map.java
+++ src/view/Map.java
@@ -3,6 +3,20 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Toolkit;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Map {
 
@@ -36,8 +50,31 @@ public class Map {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 1200, 800);
+		frame.getContentPane().setBackground(new Color(0, 0, 0));
+		frame.setResizable(false);
+		frame.setForeground(new Color(255, 255, 255));
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\jonas\\Downloads\\iconLol.png"));
+		frame.setBounds(100, 100, 950, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(170, 0, 766, 573);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\jonas\\Downloads\\RuneTerraMap.png"));
+		frame.getContentPane().add(lblNewLabel);
+		
+		JTextArea txtrChampions = new JTextArea();
+		txtrChampions.setText("CHAMPIONS");
+		txtrChampions.setForeground(Color.WHITE);
+		txtrChampions.setFont(new Font("Dubai", Font.BOLD, 23));
+		txtrChampions.setBackground(Color.BLACK);
+		txtrChampions.setBounds(21, 10, 150, 43);
+		frame.getContentPane().add(txtrChampions);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\jonas\\Downloads\\SonnaIcon.png"));
+		lblNewLabel_1.setBounds(25, 63, 120, 100);
+		frame.getContentPane().add(lblNewLabel_1);
 	}
-
 }
